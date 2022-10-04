d54a8b7c546b598b4db57f584b90c735d44b2212
Emily Pisaygnane <episaygnane@me.com>
2022-10-03 18:10:59 -0700

    created context for game state to consolidate varibles in app

---
+++ src/context/GameContext.js
@@ -0,0 +1,46 @@
+import { createContext, useContext, useState } from 'react';
+
+const GameContext = createContext();
+
+const GameProvider = ({ children }) => {
+  const [deck, setDeck] = useState(initalCards);
+  const [playerOneHand, setPlayerOneHand] = useState([]);
+  const [selectedCard, setSelectedCard] = useState();
+  const [playerTwoHand, setPlayerTwoHand] = useState([]);
+  const [playerThreeHand, setPlayerThreeHand] = useState([]);
+  const [from, setFrom] = useState('deck');
+  const [to, setTo] = useState(1);
+
+  return (
+    <GameContext.Provider
+      value={{
+        deck,
+        setDeck,
+        playerOneHand,
+        setPlayerOneHand,
+        selectedCard,
+        setSelectedCard,
+        playerTwoHand,
+        setPlayerTwoHand,
+        playerThreeHand,
+        setPlayerThreeHand,
+        from,
+        setFrom,
+        to,
+        setTo,
+      }}
+    >
+      {children}
+    </GameContext.Provider>
+  );
+};
+
+const useGame = () => {
+  const context = useContext(GameContext);
+  if (context === undefined) {
+    throw new Error('useGame must be contained within a GameProvider');
+  }
+  return context;
+};
+
+export { GameProvider, useGame };
